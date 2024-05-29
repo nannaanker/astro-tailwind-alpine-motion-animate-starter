fdb15fa97668ec0a351ee4b391faa8a2bf23638b
Emilie Skjoldann <emilie_skjoldann@outlook.dk>
2024-05-29 13:20:20 +0200

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,7 @@ module.exports = {
             fontFamily: {
 
                 'roboto': ['"Roboto", sans-serif;'],
+                customFont: ['"grotesk"']
                 
 
                 
